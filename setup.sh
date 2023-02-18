936e1f07c74540b212873049d068879f739f4eb7
thatanjan <thatanjan@gmail.com>
2023-02-18 13:21:18 +0600

    Only setup simple bar

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Install script for polybar themes
 
 # Dirs
-DIR=`pwd`
+DIR=$(pwd)
 FDIR="$HOME/.local/share/fonts"
 PDIR="$HOME/.config/polybar"
 
@@ -19,9 +19,15 @@ install_themes() {
 	if [[ -d "$PDIR" ]]; then
 		echo -e "[*] Creating a backup of your polybar configs..."
 		mv "$PDIR" "${PDIR}.old"
-		{ mkdir -p "$PDIR"; cp -rf $DIR/$STYLE/* "$PDIR"; }
+		{
+			mkdir -p "$PDIR"
+			cp -rf $DIR/$STYLE/* "$PDIR"
+		}
 	else
-		{ mkdir -p "$PDIR"; cp -rf $DIR/$STYLE/* "$PDIR"; }	
+		{
+			mkdir -p "$PDIR"
+			cp -rf $DIR/$STYLE/* "$PDIR"
+		}
 	fi
 	if [[ -f "$PDIR/launch.sh" ]]; then
 		echo -e "[*] Successfully Installed.\n"
@@ -35,29 +41,10 @@ install_themes() {
 # Main
 main() {
 	clear
-	cat <<- EOF
-		[*] Installing Polybar Themes...
-		
-		[*] Choose Style -
-		[1] Simple
-		[2] Bitmap
-	
-	EOF
 
-	read -p "[?] Select Option : "
-
-	if [[ $REPLY == "1" ]]; then
-		STYLE='simple'
-		install_fonts
-		install_themes
-	elif [[ $REPLY == "2" ]]; then
-		STYLE='bitmap'
-		install_fonts
-		install_themes
-	else
-		echo -e "\n[!] Invalid Option, Exiting...\n"
-		exit 1
-	fi
+	STYLE='simple'
+	install_fonts
+	install_themes
 }
 
 main
